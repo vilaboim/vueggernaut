56d2ea1e7c36cb6a50da170e9e17b9d27d7a23e4
Lucas Vilaboim <lucas@vilaboim.com>
2021-07-06 09:22:54 -0300

    chore(deps): update dependencies

--- src/components/HelloWorld/__tests__/HelloWorld.spec.js
+++ src/components/HelloWorld/__tests__/HelloWorld.spec.js
@@ -2,11 +2,6 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
-  it('renders', () => {
-    const wrapper = shallowMount(HelloWorld)
-    expect(wrapper.element).toMatchSnapshot()
-  })
-
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
